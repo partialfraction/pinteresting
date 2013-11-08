9b7cf87003259d58d2b684f0055aa721ef58ff12
Kevin Brown <kbrown@partialfraction.com>
2013-11-08 16:12:49 -0500

    Add Amazon S3 for Paperclip to upload to Amazon S3

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '4.0.0'
 gem 'sass-rails', '~> 4.0.0'
 gem "devise", "~> 3.1.1"
 gem "paperclip", "~> 3.0"
+gem "aws-sdk", "~> 1.24.0"
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Pinteresting::Application.configure do
   # Required for Devise
   # Set to some hostname
   config.action_mailer.default_url_options = { :host => 'kb-pinteresting.herokuapps.com' }
+
+  # This sets paperclip to upload to Amazon bucket S3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
